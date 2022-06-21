dc7ff88e7642dcfa54c828afb9dffc3c1a4e5db6
nix@mail.de <nix@mail.de>
2022-06-21 18:55:53 +0200

    test

---
+++ Beispiel/HMI/farberkennung/farberkennung_mitEvent.cnv.cs
@@ -0,0 +1,44 @@
+﻿/*
+ * Created by EcoStruxure Automation Expert.
+ * User: labor
+ * Date: 27/04/2022
+ * Time: 8:25 am
+ * 
+ */
+
+using System;
+using NxtControl.GuiFramework;
+
+namespace HMI.Main.Symbols.farberkennung
+{
+	/// <summary>
+	/// Description of mitEvent.
+	/// </summary>
+	public partial class mitEvent : NxtControl.GuiFramework.HMISymbol
+	{
+		public mitEvent()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			this.REQ_Fired += REQ_Fired_EventHandler;
+		}
+
+	
+
+		void DrawnButton1Click(object sender, EventArgs e)
+		{
+			// TODO: Implement DrawnButton1Click
+			FireEvent_CNF();
+		}
+
+		void REQ_Fired_EventHandler(object sender, REQEventArgs e)
+		{
+			// TODO: Implement REQ_Fired_EventHandler
+			twoStateButton1.Checked = true;
+				
+	
+		}
+	}
+}
